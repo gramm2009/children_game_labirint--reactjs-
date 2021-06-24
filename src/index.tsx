1eaba2085a763fcaf966dd2c14357ca6d0521422
gramm2009 <viktorkaturkin@gmail.com>
2021-06-24 15:16:49 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+
+import { BrowserRouter } from 'react-router-dom';
 import Router from './router/router';
 import configureStore from './redux/store';
 import { Provider } from 'react-redux';
 
-const store = configureStore()
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={ store }>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById( 'root' )
-);
+    <Provider store={store}>
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+    </Provider>,
 
-reportWebVitals();
+    document.getElementById('root')
+);
